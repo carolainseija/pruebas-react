8e5559225582b3ac1fa23bcee3b37ae5eb76ffeb
carolainseija <carolainsilva1@gmail.com>
2022-03-28 11:29:56 -0300

    form otra forma de usar const

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import classes from './Form.module.css';
+import Input from './Input';
 const Form = (props) => {
 
+
+    //como funcion-componente
     const Title = () => <h1>{props.text2}</h1>
-    const Parrafo = () => <p>{props.par}</p>
+    //como contante
+    const Parrafo = <p>{props.par}</p>
 
     return (
         <form className={classes.form}>
             {/* Las props de estas const se aaplican en el componente pare "form" */}
+            {/* muesto las funciones o componentes de esta manera */}
             <Title />
-            <Parrafo />
+            {/*muestro la constante de esta manera */}
+            {Parrafo}
             <input id='input-text' name='input1' type="text" placeholder="textos." />
             <input />
             {props.children}
